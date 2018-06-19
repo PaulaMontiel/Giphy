c5cbe1c90ff48f54be822418497dbecc9605221c
Paula Montiel <p_dany_mr@hotmail.com>
2018-06-19 00:15:05 -0400

    haciendo cambios

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,7 +7,7 @@ inputText.addEventListener("keypress",(event) => {
          let gif = inputText.value;
          inputText.value= " ";
          containerImage.innerHTML= " ";
-         fetch(`https://api.giphy.com/v1/gifs/search?api_key=3SwqSH7M9VKat0YYdowYwDEL0q30Ajrv`)
+         fetch(`https://api.giphy.com/v1/stickers/search?api_key=3SwqSH7M9VKat0YYdowYwDEL0q30Ajrv&q=${gif}&limit=25&offset=0&rating=G&lang=en`)
          .then (response => response.json())
          .then (data =>{
            renderInfo(data);
